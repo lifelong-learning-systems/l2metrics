e73bb7ca6e91df204f837bfe96af9f32e8be5c79
Clay Fink <clayfink@gmail.com>
2019-10-28 13:47:43 -0400

    Added l2metrics.util.read_log_data.

--- l2metrics/util.py
+++ l2metrics/util.py
@@ -1,9 +1,27 @@
 import pandas as pd
+import os
 
-def load_logdata(log_rootdir):
-    """
-    Sample utility function
-    """
-    df = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
-         columns=['a', 'b', 'c'])
-    return df
+def read_log_data(dir, analysis_variables=None):
+    logs = None
+    blocks = None
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            if file == 'data-log.tsv':
+                task = os.path.split(root)[-1]
+                if analysis_variables is not None:
+                    df = pd.read_csv(os.path.join(root, file), sep='\t')[
+                        ['timestamp', 'class_name', 'phase', 'worker', 'block', 'task', 'seed'] + analysis_variables]
+                else:
+                    df = pd.read_csv(os.path.join(root, file), sep='\t')
+                if logs is None:
+                    logs = df
+                else:
+                    logs = pd.concat([logs, df])
+            if file == 'block-report.tsv':
+                df = pd.read_csv(os.path.join(root, file), sep='\t')
+                if blocks is None:
+                    blocks = df
+                else:
+                    blocks = pd.concat([blocks, df])
+
+    return logs.merge(blocks, on = ['phase', 'class_name', 'worker', 'block'])

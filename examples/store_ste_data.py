b9d05c395c9fe83f241c40794c44ebee075319bc
Eric Nguyen <Eric.Nguyen@jhuapl.edu>
2020-09-15 00:18:12 -0400

    Implement script for storing STE data

---
+++ examples/store_ste_data.py
@@ -0,0 +1,48 @@
+import argparse
+from l2metrics import _localutil, util
+import traceback
+
+
+def run():
+    parser = argparse.ArgumentParser(description='Store single task expert data from the command line')
+
+    # We assume that the logs are found in a subdirectory under $L2DATA/logs
+    # This subdirectory must be passed as a parameter in order to locate the logs
+    # which will be parsed by this code
+    parser.add_argument('-log_dir', default=None, help='Subdirectory under $L2DATA/logs for the log files')
+
+    args = parser.parse_args()
+
+    if args.log_dir is None:
+        raise Exception('Log directory must be specified!')
+
+    # Load data from ste logs
+    log_data = util.read_log_data(args.log_dir)
+    log_data = log_data.sort_values(
+        by=['block', 'task']).set_index("block", drop=False)
+
+    # Get task name
+    task_name = log_data.class_name.unique()
+
+    # Check for number of tasks in scenario
+    if task_name.size != 1:
+        raise Exception('Scenario does not only contain one task!')
+
+    # Get base directory to store ste data
+    filename = util.get_l2root_base_dirs('taskinfo', task_name[0] + '.pkl')
+
+    # Store ste data in task info directory
+    log_data.to_pickle(filename)
+
+    print('Stored STE data for', task_name[0])
+
+    # Plot data
+    util.plot_performance(log_data, do_smoothing=True, do_task_colors=True, do_save_fig=False,
+                          max_smoothing_window=100, input_title=args.log_dir)
+
+
+if __name__ == "__main__":
+    try:
+        run()
+    except:
+        traceback.print_exc()

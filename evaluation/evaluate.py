e401577a7f1633cf048b9fe02d03682391b58bed
Eric Nguyen <Eric.Nguyen@jhuapl.edu>
2021-05-03 12:39:26 -0400

    Update documentation and parameter descriptions

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -330,60 +330,62 @@ def evaluate() -> None:
 
     # Evaluation directory be absolute or relative paths
     parser.add_argument('-l', '--eval-dir', default='', type=str,
-                        help='Evaluation directory containing logs')
+                        help='Evaluation directory containing logs. Defaults to "".')
 
     # Evaluation directory be absolute or relative paths
     parser.add_argument('-s', '--ste-dir', default='', type=str,
-                        help='Agent configuration directory of STE data')
+                        help='Agent configuration directory of STE data. Defaults to "".')
 
     # Method for handling multiple STE runs
     parser.add_argument('-v', '--ste-averaging-method', default='time', choices=['time', 'metrics'],
-                        help='Method for handling STE runs, time-series averaging (time) or'
-                        'LL metric averaging (metric)')
+                        help='Method for handling STE runs, time-series averaging (time) or LL \
+                            metric averaging (metrics). Defaults to time.')
 
     # Choose application measure to use as performance column
-    parser.add_argument('-p', '--perf-measure', default='performance', type=str,
-                        help='Name of column to use for metrics calculations')
+    parser.add_argument('-p', '--perf-measure', default='reward', type=str,
+                        help='Name of column to use for metrics calculations. Defaults to reward.')
 
     # Method for aggregating within-lifetime metrics
-    parser.add_argument('-a', '--aggregation-method', default='mean', choices=['mean', 'median'],
-                        help='Method for aggregating within-lifetime metrics')
+    parser.add_argument('-a', '--aggregation-method', default='mean', type=str, choices=['mean', 'median'],
+                        help='Method for aggregating within-lifetime metrics. Defaults to mean.')
 
     # Method for calculating performance maintenance
-    parser.add_argument('-m', '--maintenance-method', default='mrlep', choices=['mrtlp', 'mrlep', 'both'],
-                        help='Method for computing performance maintenance')
+    parser.add_argument('-m', '--maintenance-method', default='mrlep', type=str, choices=['mrlep', 'mrtlp', 'both'],
+                        help='Method for computing performance maintenance. Defaults to mrlep.')
 
     # Method for calculating forward and backward transfer
-    parser.add_argument('-t', '--transfer-method', default='ratio', choices=['ratio', 'contrast', 'both'],
-                        help='Method for computing forward and backward transfer')
+    parser.add_argument('-t', '--transfer-method', default='ratio', type=str, choices=['ratio', 'contrast', 'both'],
+                        help='Method for computing forward and backward transfer. Defaults to ratio.')
 
     # Method for normalization
-    parser.add_argument('-n', '--normalization-method', default='task', choices=['task', 'run'],
-                        help='Method for normalizing data')
+    parser.add_argument('-n', '--normalization-method', default='task', type=str, choices=['task', 'run', 'none'],
+                        help='Method for normalizing data. Defaults to task.')
 
     # Method for smoothing
-    parser.add_argument('-g', '--smoothing-method', default='flat', choices=['none', 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'],
-                        help='Method for smoothing data')
+    parser.add_argument('-g', '--smoothing-method', default='flat', type=str, choices=['flat', 'hanning', 'hamming', 'bartlett', 'blackman', 'none'],
+                        help='Method for smoothing data, window type. Defaults to flat.')
 
     # Window length for smoothing
     parser.add_argument('-w', '--window-length', default=None, type=int,
-                        help='Window length for smoothing data')
+                        help='Window length for smoothing data. Defaults to None.')
 
     # Flag for removing outliers
     parser.add_argument('-x', '--clamp-outliers', action='store_true',
-                        help='Remove outliers in data for metrics by clamping to quantiles')
+                        help='Remove outliers in data for metrics by clamping to quantiles. Defaults \
+                            to false.')
 
     # Data range file for normalization
     parser.add_argument('-d', '--data-range-file', default=None, type=str,
-                        help='JSON file containing task performance ranges for normalization')
+                        help='JSON file containing task performance ranges for normalization. \
+                            Defaults to None.')
 
     # Output directory
     parser.add_argument('-O', '--output-dir', default='results', type=str,
-                        help='Directory for output files')
+                        help='Directory for output files. Defaults to results.')
 
-    # Output file location
+    # Output filename
     parser.add_argument('-o', '--output', default='ll_metrics', type=str,
-                        help='Output filename for results')
+                        help='Output filename for results. Defaults to ll_metrics.')
 
     # Flag for enabling unzipping of logs
     parser.add_argument('-u', '--do-unzip', action='store_true',
@@ -391,43 +393,44 @@ def evaluate() -> None:
 
     # Flag for showing raw performance data under smoothed data
     parser.add_argument('-r', '--show-raw-data', action='store_true',
-                        help='Show raw data points under smoothed data for plotting')
+                        help='Show raw data points under smoothed data for plotting. Defaults to \
+                            false.')
 
     # Flag for showing evaluation block lines
     parser.add_argument('-e', '--show-eval-lines', dest='show_eval_lines', default=True, action='store_true',
-                        help='Show lines between evaluation blocks')
+                        help='Show lines between evaluation blocks. Defaults to true.')
     parser.add_argument('--no-show-eval-lines', dest='show_eval_lines', action='store_false',
                         help='Do not show lines between evaluation blocks')
 
     # Flag for disabling STE save
     parser.add_argument('-T', '--do-store-ste', dest='do_store_ste', default=True, action='store_true',
-                        help='Store STE data')
+                        help='Store STE data. Defaults to true.')
     parser.add_argument('--no-store-ste', dest='do_store_ste', action='store_false',
                         help='Do not store STE data')
 
     # Flag for enabling/disabling plotting
     parser.add_argument('-P', '--do-plot', dest='do_plot', default=True, action='store_true',
-                        help='Plot performance')
+                        help='Plot performance. Defaults to true.')
     parser.add_argument('--no-plot', dest='do_plot', action='store_false',
                         help='Do not plot performance')
 
     # Flag for enabling plot save
     parser.add_argument('-L', '--do-save-plots', dest='do_save_plots', default=True, action='store_true',
-                        help='Save scenario and STE plots')
+                        help='Save scenario and STE plots. Defaults to true.')
     parser.add_argument('--no-save-plots', dest='do_save_plots', action='store_false',
                         help='Do not save scenario and STE plots')
 
     # Flag for enabling/disabling save
     parser.add_argument('-S', '--do-save', dest='do_save', default=True, action='store_true',
-                        help='Save metrics outputs')
+                        help='Save metrics outputs. Defaults to true.')
     parser.add_argument('--no-save', dest='do_save', action='store_false',
                         help='Do not save metrics outputs')
 
     # Settings file arguments
-    parser.add_argument('-c', '--load-settings', default='', type=str,
-                        help='Load evaluation settings from JSON file')
+    parser.add_argument('-c', '--load-settings', default=None, type=str,
+                        help='Load evaluation settings from JSON file. Defaults to None.')
     parser.add_argument('-C', '--do-save-settings', dest='do_save_settings', default=True, action='store_true',
-                        help='Save L2Metrics settings to JSON file')
+                        help='Save L2Metrics settings to JSON file. Defaults to true.')
     parser.add_argument('--no-save-settings', dest='do_save_settings', action='store_false',
                         help='Do not save L2Metrics settings to JSON file')
 

8d10e981bc2e0e91a30cf82ac482185bd0547b64
Eric Nguyen <Eric.Nguyen@jhuapl.edu>
2021-10-18 10:40:35 -0400

    Update CHANGELOG and setup.py (v2.11.0)

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ HERE = pathlib.Path(__file__).parent
 
 setup(
     name='l2metrics',
-    version='2.12.0',
+    version='2.13.0',
     description='Metrics for Lifelong Learning',
     long_description=(HERE / 'README.md').read_text(),
     long_description_content_type='text/markdown',
